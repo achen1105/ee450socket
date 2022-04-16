324360f2ade675e24034374b8485148225d54705
achen1105 <achen1105@gmail.com>
2022-04-16 03:48:38 -0700

    make servers communicate

--- clientA.cpp
+++ clientA.cpp
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <iostream>
 #include <errno.h>
 #include <string.h>
@@ -8,9 +11,68 @@
 #include <arpa/inet.h>
 #include <sys/wait.h>
 
-using namespace std;
+// 6.2 A Simple Stream Client
+/*
+** talker.c -- a datagram "client" demo
+*/
+
+#define SERVERPORT "24421"    // the port users will be connecting to
+
+int main(int argc, char *argv[])
+{
+    int sockfd;
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    int numbytes;
+
+    if (argc != 3) {
+        fprintf(stderr,"usage: talker hostname message\n");
+        exit(1);
+    }
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET6; // set to AF_INET to use IPv4
+    hints.ai_socktype = SOCK_DGRAM;
+
+    if ((rv = getaddrinfo(argv[1], SERVERPORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    // loop through all the results and make a socket
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("talker: socket");
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "talker: failed to create socket\n");
+        return 2;
+    }
+
+    printf("The clientA is up and running.\n");
+
+    if ((numbytes = sendto(sockfd, argv[2], strlen(argv[2]), 0,
+             p->ai_addr, p->ai_addrlen)) == -1) {
+        perror("talker: sendto");
+        exit(1);
+    }
+
+    freeaddrinfo(servinfo);
+
+    printf("talker: sent %d bytes to %s\n", numbytes, argv[1]);
+    close(sockfd);
+
+    return 0;
+}
 
 
+/**
 int main(int argc, char** argv)
 {
     // 5.1
@@ -23,12 +85,11 @@ int main(int argc, char** argv)
     hints.ai_socktype = SOCK_STREAM;
     // hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
 
-    /**
-    int getaddrinfo(const char *node,     // e.g. "www.example.com" or IP
-                    const char *service,  // e.g. "http" or port number
-                    const struct addrinfo *hints,
-                    struct addrinfo **res);
-    */
+    //int getaddrinfo(const char *node,     // e.g. "www.example.com" or IP
+     //               const char *service,  // e.g. "http" or port number
+       //             const struct addrinfo *hints,
+         //           struct addrinfo **res);
+    
     getaddrinfo("127.0.0.1", "24421", &hints, &res);
 
     // make a socket:
@@ -38,4 +99,7 @@ int main(int argc, char** argv)
     // connect!
     // int connect(int sockfd, struct sockaddr *serv_addr, int addrlen); 
     connect(sockfd, res->ai_addr, res->ai_addrlen);
+
+    cout << "The client A is up and running." << endl;
 }
+*/
--- serverM.cpp
+++ serverM.cpp
@@ -1,4 +1,6 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <errno.h>
 #include <string.h>
 #include <netdb.h>
@@ -7,35 +9,96 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <sys/wait.h>
+#include <signal.h>
 
-using namespace std;
+// Adapted from 6.1 A Simple Stream Server
+/*
+** listener.c -- a datagram sockets "server" demo
+*/
 
+#define MYNODE "127.0.0.1" // local host hardcoded
+#define MYPORT "24421"    // the port users will be connecting to
 
-int main(int argc, char** argv)
+#define MAXBUFLEN 100 // max buffer length
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
 {
-    // 5.1
-    struct addrinfo hints, *res;
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int main(void)
+{
     int sockfd;
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    int numbytes;
+    struct sockaddr_storage their_addr;
+    char buf[MAXBUFLEN];
+    socklen_t addr_len;
+    char s[INET6_ADDRSTRLEN];
 
-    // first, load up address structs with getaddrinfo():
     memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;  // use IPv4 or IPv6, whichever
-    hints.ai_socktype = SOCK_STREAM;
-    // hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
+    hints.ai_family = AF_INET6; // set to AF_INET to use IPv4
+    hints.ai_socktype = SOCK_DGRAM;
+    //hints.ai_flags = AI_PASSIVE; // use my IP
 
-    /**
-    int getaddrinfo(const char *node,     // e.g. "www.example.com" or IP
-                    const char *service,  // e.g. "http" or port number
-                    const struct addrinfo *hints,
-                    struct addrinfo **res);
-    */
-    getaddrinfo("127.0.0.1", "24421", &hints, &res);
+    // check errors in address info
+    if ((rv = getaddrinfo(MYNODE, MYPORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
 
-    // make a socket:
-    // int socket(int domain, int type, int protocol); 
-    sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    // loop through all the results and bind to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("listener: socket");
+            continue;
+        }
 
-    // bind it to the port we passed in to getaddrinfo():
-    // int bind(int sockfd, struct sockaddr *my_addr, int addrlen);
-    bind(sockfd, res->ai_addr, res->ai_addrlen);
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("listener: bind");
+            continue;
+        }
+
+        break;
+    }
+    
+    // error in binding socket
+    if (p == NULL) {
+        fprintf(stderr, "listener: failed to bind socket\n");
+        return 2;
+    }
+
+    freeaddrinfo(servinfo);
+
+    // successful socket
+    printf("The main server is up and running.\n");
+
+    // check buffer length
+    addr_len = sizeof their_addr;
+    if ((numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1 , 0,
+        (struct sockaddr *)&their_addr, &addr_len)) == -1) {
+        perror("recvfrom");
+        exit(1);
+    }
+
+    // 
+    printf("listener: got packet from %s\n",
+        inet_ntop(their_addr.ss_family,
+            get_in_addr((struct sockaddr *)&their_addr),
+            s, sizeof s));
+    printf("listener: packet is %d bytes long\n", numbytes);
+    buf[numbytes] = '\0';
+    printf("listener: packet contains \"%s\"\n", buf);
+
+    //close(sockfd);
+
+    return 0;
 }
